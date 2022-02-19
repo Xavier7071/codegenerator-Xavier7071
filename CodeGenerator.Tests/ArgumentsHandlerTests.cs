622d81886051dbd4aaf5442a1123147c4ae3f23e
Xavier Mathieu <xavier2003@hotmail.fr>
2022-02-19 14:01:16 -0500

    Ajout tests ArgumentHandler

--- CodeGenerator.Tests/ArgumentsHandlerTests.cs
+++ CodeGenerator.Tests/ArgumentsHandlerTests.cs
@@ -4,8 +4,25 @@ namespace CodeGenerator.Tests;
 
 public class ArgumentsHandlerTests
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("-n", "-f", "test.json", "-c", "csharp", "-v")]
+    [InlineData("-f", "test.json")]
+    [InlineData("-n", "class", "-f")]
+    [InlineData("-n", "class", "-f", "test.json", "-c")]
+    public void HandleArgumentsWithError(params string[] args)
     {
+        var argumentsHandler = new ArgumentsHandler(args);
+        Assert.True(argumentsHandler.HasError);
+    }
+
+    [Theory]
+    [InlineData("-n", "class", "-f", "test.json", "-c", "csharp")]
+    [InlineData("-f", "test.json", "-n", "class", "-v")]
+    [InlineData("-v", "-c", "csharp", "-f", "test.json", "-n", "class")]
+    [InlineData("-n", "class", "-f", "test.json")]
+    public void HandleArgumentsWithoutError(params string[] args)
+    {
+        var argumentsHandler = new ArgumentsHandler(args);
+        Assert.False(argumentsHandler.HasError);
     }
 }
