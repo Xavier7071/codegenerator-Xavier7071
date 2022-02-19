9f65eb9e739c76f51c176cc056447b4e6ce9ee11
Xavier Mathieu <xavier2003@hotmail.fr>
2022-02-19 14:03:33 -0500

    Ajout parser tests

--- CodeGenerator.Tests/ParserTests.cs
+++ CodeGenerator.Tests/ParserTests.cs
@@ -5,7 +5,16 @@ namespace CodeGenerator.Tests;
 public class ParserTests
 {
     [Fact]
-    public void Test1()
+    public void ValidFile()
     {
+        var parser = new Parser("fa.json");
+        Assert.False(parser.HasError);
+    }
+
+    [Fact]
+    public void InvalidFile()
+    {
+        var parser = new Parser("test.json");
+        Assert.True(parser.HasError);
     }
 }
