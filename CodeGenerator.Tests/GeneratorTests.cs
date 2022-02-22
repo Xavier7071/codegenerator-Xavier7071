c9542fe1a605da4fb05ca424e366a87212777cc5
Xavier Mathieu <xavier2003@hotmail.fr>
2022-02-22 10:58:37 -0500

    Création de la classe pour tests

--- CodeGenerator.Tests/GeneratorTests.cs
+++ CodeGenerator.Tests/GeneratorTests.cs
@@ -4,10 +4,4 @@ namespace CodeGenerator.Tests;
 
 public class GeneratorTests
 {
-    [Fact]
-    public void ValidFile()
-    {
-        var parser = new Parser("fa.json");
-        Assert.False(parser.HasError);
-    }
 }
